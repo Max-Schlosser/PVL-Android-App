1216ff408f57a75d01fbbccbbfd5240d2f5ece04
StephanWagener <stephan-wagener@web.de>
2015-05-31 21:22:55 +0200

    2. Verfahren fehlt noch.

--- QuanPic/app/src/main/java/com/example/StephanWagener/quanpic/MainActivity.java
+++ QuanPic/app/src/main/java/com/example/StephanWagener/quanpic/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.StephanWagener.quanpic;
 
-// Imports needed for used functionalities.
-
+// Importe für die verschiedenen Funktionalitäten.
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Environment;
@@ -15,7 +14,6 @@ import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
-import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.highgui.Highgui;
 import org.opencv.imgproc.Imgproc;
@@ -24,14 +22,25 @@ import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-// Main Class. Links layout and implementation.
+// Die Hauptklasse, die beim Start erstellt und initialisiert wird.
+// Hier werden die Funktionen eingebunden und es wird auf bestimmte
+// Aktivitäten des Nutzers reagiert.
 public class MainActivity extends Activity {
 
-    // Global variables
+    // Globale Variablen für die App
+    //Das GUI-Element zur Anzeige der quantisierten Bilder.
     private CameraBridgeViewBase cameraView;
-    private boolean isMedianCut = true;
+
+    //Das Flag zur Änderung des Verfahrens.
+    private Boolean isMedianCut = null;
+
+    //Ein Zeitstempel, um die Zeit des Haltens des Fingers auf dem Bildschirm zu messen.
     long time = 0;
+
+    //Das aktuell angezeigte Bild, dass dann gespeichert werden kann.
     private Mat currentInput;
+
+    //Initialisierung und Aktivierung des Frames zuständig. Den OpenCVManager einbinden.
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
@@ -48,7 +57,7 @@ public class MainActivity extends Activity {
         }
     };
 
-    // Initializing required elements when starting the application.
+    // Initialisierung wichtiger Elemente und setzen des Layouts mit Listenern.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,9 +69,12 @@ public class MainActivity extends Activity {
         setFunctionality();
     }
 
+    //Setzen der Listener und einbinden der Funktionen, der dann eintretenden Aktionen.
     private void setFunctionality()
     {
         cameraView.setVisibility(SurfaceView.VISIBLE);
+
+        //Überschreiben des Frames mit dem ausgewählten Verfahren.
         cameraView.setCvCameraViewListener(new CameraBridgeViewBase.CvCameraViewListener2() {
 
             @Override
@@ -74,24 +86,32 @@ public class MainActivity extends Activity {
             @Override
             public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame)
             {
-                if (isMedianCut)
+                if (isMedianCut == null)
                 {
-                    Mat convertedMat = new Mat();
+                    //Kein Verfahren.
+                    currentInput = inputFrame.rgba();
+                    return inputFrame.rgba();
+                }
+                else if (isMedianCut)
+                {
+                    //Median Filter.
                     Mat medianFilteredMat = new Mat();
-                    inputFrame.rgba().convertTo(convertedMat, CvType.CV_16U);
-                    Imgproc.medianBlur(convertedMat, medianFilteredMat, 3);
+                    Imgproc.medianBlur(inputFrame.rgba(), medianFilteredMat, 3);
                     currentInput = medianFilteredMat;
                     return currentInput;
                 }
                 else
                 {
-                    //TODO population implementation
-                    currentInput = inputFrame.rgba();
-                    return inputFrame.rgba();
+                    //Popularitätsverfahren.
+                    //TODO Pop
+                    return null;
                 }
             }
         });
 
+        //Funktionalitäten, die durch Touch ausgelöst werden.
+        //Beim tippen wird gespeichert und beim Halten auf den
+        //Bildschirm wird das Verfahren gewechseln.
         cameraView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event)
@@ -105,12 +125,27 @@ public class MainActivity extends Activity {
                 {
                     if (event.getEventTime() - time > 1500)
                     {
-                        isMedianCut = !isMedianCut;
-                        if (isMedianCut)
+                        if (isMedianCut == null)
+                        {
+                            isMedianCut = true;
+                        }
+                        else if (isMedianCut)
+                        {
+                            isMedianCut = false;
+                        }
+                        else
+                        {
+                            isMedianCut = null;
+                        }
+                        if (isMedianCut == null)
+                        {
+                            Toast.makeText(getApplicationContext(), "Kein Verfahren ist aktiviert.", Toast.LENGTH_SHORT).show();
+                        }
+                        else if (isMedianCut)
                         {
                             Toast.makeText(getApplicationContext(), "Das \"Median-Cut-Verfahren\" wurde aktiviert.", Toast.LENGTH_SHORT).show();
                         }
-                        else
+                        else if (!isMedianCut)
                         {
                             Toast.makeText(getApplicationContext(), "Das \"Popularitätsverfahren\" wurde aktiviert.", Toast.LENGTH_SHORT).show();
                         }
@@ -126,10 +161,12 @@ public class MainActivity extends Activity {
         });
     }
 
-    public void saveImage (Mat mat) {
+    //Das Speichern des momentan angezeigten Bildes als PNG-File im Ordner QuanPic unter Pictures.
+    public void saveImage (Mat mat)
+    {
         Mat mIntermediateMat = new Mat();
         Imgproc.cvtColor(mat, mIntermediateMat, Imgproc.COLOR_RGBA2BGRA, 3);
-        File path = new File(Environment.getExternalStorageDirectory() + "/Images/");
+        File path = new File(Environment.getExternalStorageDirectory() + "/Pictures/QuanPic/");
         path.mkdirs();
 
         String filename = "quanpic" + new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss").format(new Date()) + ".png";
@@ -143,6 +180,7 @@ public class MainActivity extends Activity {
             Toast.makeText(getApplicationContext(), "Beim Speichern des Bildes ist ein Fehler aufgetreten!", Toast.LENGTH_SHORT).show();
     }
 
+    //Override-Methoden, um die OneCvView zu schließen oder zu pausieren.
     @Override
     public void onPause()
     {
@@ -158,6 +196,7 @@ public class MainActivity extends Activity {
             cameraView.disableView();
     }
 
+    //Laden des OpenCvManagers.
     @Override
     public void onResume()
     {
